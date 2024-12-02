d2dfbc08fe129b82ad8afdd9e820b23499083cde
karthickvikraman <vikramkarthick5@gmail.com>
2024-12-02 12:00:48 +0530

    Updated version-1

--- src/components/Education.js
+++ src/components/Education.js
@@ -4,23 +4,28 @@ export default function Education(){
            <div className="md:px-20 my-5 mx-5">
              <p className="text-4xl border-b-2 border-white w-[165px] font-bold font-hero-font bc">Education</p>
            </div>
-           <div className="flex flex-col mx-20 mt-10 h-[450px] border-l">
+           <div className="flex flex-col mx-20 md:mx-28 mt-10 h-[450px] border-l">
                <div className="flex">
-                  <div className="mt-[100px] border-t w-[100px] border-white"></div>
+                  <p className="ml-2 mt-[93px] text-white text-xs font-hero-font">2024</p>
+                  <div className="ml-2 mt-[100px] border-t w-[100px] border-white"></div>
                   <div className="edu">
                      <p className="font-hero-font text-lg text-white">GCE-Tirunelveli<br/><span className="text-sm text-white font-hero-font">BE-CSE</span></p>
                      <p className="text-sm font-hero-font text-white">Percenage-83</p>
                   </div>
                </div>
+
                <div className="flex">
-                  <div className="mt-[100px] border-t w-[100px] border-white"></div>
+                  <p className="ml-2 mt-[93px] text-white text-xs font-hero-font">2020</p>
+                  <div className="ml-2 mt-[100px] border-t w-[100px] border-white"></div>
                   <div className="edu">
                     <p className="font-hero-font text-lg text-white">KA Hr. Sec. School<br/><span className="text-sm text-white font-hero-font">HSC</span></p>
                     <p className="text-sm font-hero-font text-white">Percenage-82.5</p>
                   </div>
                </div>
+
                <div className="flex">
-                  <div className="mt-[100px] border-t w-[100px] border-white"></div>
+                  <p className="ml-2 mt-[93px] text-white text-xs font-hero-font">2018</p>
+                  <div className="ml-2 mt-[100px] border-t w-[100px] border-white"></div>
                   <div className="edu">
                     <p className="font-hero-font text-lg text-white">KA Hr. Sec. School<br/><span className="text-sm text-white font-hero-font">SSLC</span></p>
                     <p className="text-sm font-hero-font text-white">Percenage-78</p>
